ebbbbd6ed63e025d582da3f383f83f0bc7be76bf
wojtekbu <wbulawa@soldevelo.com>
2019-04-03 11:58:36 +0200

    AO-145: Added translation for reporting rate report due date

---
+++ src/main/resources/db/migration/201904021840934581__add_translation_for_reporting_rate_report_due_date.sql
@@ -0,0 +1,6 @@
+UPDATE reports.template_parameters
+SET displayname = 'Data de vencimento (dias)',
+    displayproperty = 'name',
+    "name" = 'DueDays',
+    selectproperty = 'name'
+WHERE id = 'aadc19e9-3a6d-4bfb-8095-418bb999b84d';
