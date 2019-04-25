36de99dd615bc2aa2c8965d53d316ecbefd9a42c
Jakub Kondrat <jkondrat@soldevelo.com>
2019-04-25 17:27:21 +0200

    AO-231: Make it possible to get translations in OrderStatusDto

--- src/main/java/org/openlmis/ao/reports/dto/external/OrderStatusDto.java
+++ src/main/java/org/openlmis/ao/reports/dto/external/OrderStatusDto.java
@@ -1,8 +1,11 @@
 package org.openlmis.ao.reports.dto.external;
 
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public enum OrderStatusDto {
   ORDERED,
@@ -24,4 +27,29 @@ public enum OrderStatusDto {
           put(READY_TO_PACK, "Pronto para embalar");
         }
       });
+
+  /**
+   * Find a correct {@link OrderStatusDto} instance based on the passed string. The method ignores
+   * the case.
+   *
+   * @param arg string representation of one of order status.
+   * @return instance of {@link OrderStatusDto} if the given string matches status; otherwise null.
+   */
+  public static OrderStatusDto fromString(String arg) {
+    for (OrderStatusDto status : values()) {
+      if (equalsIgnoreCase(arg, status.name())) {
+        return status;
+      }
+    }
+
+    return null;
+  }
+
+  public String getTranslation() {
+    return TRANSLATIONS.get(this);
+  }
+
+  public static String getTranslation(String status) {
+    return Objects.requireNonNull(fromString(status)).getTranslation();
+  }
 }
