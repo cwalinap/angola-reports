118619bf4dba5bdddcca311bf942ba3b52f1e4db
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-04-25 16:38:26 +0200

    AO-231: added translations for order statuses

--- src/main/java/org/openlmis/ao/reports/dto/external/OrderStatusDto.java
+++ src/main/java/org/openlmis/ao/reports/dto/external/OrderStatusDto.java
@@ -1,5 +1,9 @@
 package org.openlmis.ao.reports.dto.external;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum OrderStatusDto {
   ORDERED,
   FULFILLING,
@@ -8,4 +12,16 @@ public enum OrderStatusDto {
   TRANSFER_FAILED,
   IN_ROUTE,
   READY_TO_PACK;
+
+  private static final Map<OrderStatusDto, String> TRANSLATIONS =
+      Collections.unmodifiableMap(new HashMap<OrderStatusDto, String>() {{
+          put(ORDERED, "Pediu");
+          put(FULFILLING, "A executar pedido");
+          put(SHIPPED, "Enviado");
+          put(RECEIVED, "Recebido");
+          put(TRANSFER_FAILED, "Transferência sem êxito");
+          put(IN_ROUTE, "A Caminho");
+          put(READY_TO_PACK, "Pronto para embalar");
+        }
+      });
 }
