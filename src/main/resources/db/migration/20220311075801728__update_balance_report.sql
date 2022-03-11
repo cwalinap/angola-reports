7a9ce9debbd41276ba70e30895062720f03f8773
NikolaLaskowska <nicola.laskowska@gmail.com>
2022-03-11 08:59:16 +0100

    ANGOLASUP-628: Set data fields to required in balance report

---
+++ src/main/resources/db/migration/20220311075801728__update_balance_report.sql
@@ -0,0 +1,7 @@
+UPDATE reports.template_parameters
+SET required=true
+WHERE id = 'cc478c30-1736-4e0b-a0f1-fedc189712f2';
+
+UPDATE reports.template_parameters
+SET required=true
+WHERE id = 'ff64d277-07e0-4343-8da8-8f27429006e8';
