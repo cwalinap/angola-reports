1cead66eb197ba63f39210fecb25b85ee2a75e2a
NikolaLaskowska <nicola.laskowska@gmail.com>
2022-03-15 16:40:32 +0100

    AO-680: Update parameters for stock movements reports

---
+++ src/main/resources/db/migration/20220315153227626__update_stock_movements_reports.sql
@@ -0,0 +1,15 @@
+UPDATE reports.template_parameters
+SET name = 'program'
+WHERE id = 'bfd54272-941e-47cb-b2b4-358d547ee328';
+
+UPDATE reports.template_parameters
+SET name = 'facility'
+WHERE id = '35dd6fbd-e22c-4d7d-ac08-d6193e77aa96';
+
+UPDATE reports.template_parameters
+SET name = 'program'
+WHERE id = '5c757abd-83a2-4993-8ea0-6793762b0f41';
+
+UPDATE reports.template_parameters
+SET name = 'facility'
+WHERE id = '010570aa-7e58-45c3-8f85-754e464af742';
