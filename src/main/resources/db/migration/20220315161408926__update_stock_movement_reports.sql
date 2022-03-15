ee104e2df60327e9c38c35653e21e477a90f3b83
NikolaLaskowska <nicola.laskowska@gmail.com>
2022-03-15 17:15:24 +0100

    AO-680: Update parameters for stock movements reports

---
+++ src/main/resources/db/migration/20220315161408926__update_stock_movement_reports.sql
@@ -0,0 +1,7 @@
+UPDATE reports.template_parameters
+SET displayproperty='fullProductName', name='productId'
+WHERE id = 'c83d4fc6-9028-453f-93e1-58ee5e0f6a08';
+
+UPDATE reports.template_parameters
+SET displayproperty='fullProductName', name='productId'
+WHERE id = 'b46fed0f-971a-4107-91d2-aeed8528ab98';
