835f8fc896d2219f2677b3eab5cfd71d192a86a2
NikolaLaskowska <nicola.laskowska@gmail.com>
2022-04-05 13:02:15 +0200

    AO-679: Fix displaying programs for stock movement reports

---
+++ src/main/resources/db/migration/20220405102813988__update_stock_movement_reports.sql
@@ -0,0 +1,7 @@
+UPDATE reports.template_parameters
+SET displayproperty='name'
+WHERE id = 'bfd54272-941e-47cb-b2b4-358d547ee328';
+
+UPDATE reports.template_parameters
+SET displayproperty='name'
+WHERE id = '5c757abd-83a2-4993-8ea0-6793762b0f41';
