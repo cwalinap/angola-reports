2b1763942e05f0f048b413a4d7bc739bd6552966
ptrrk1337 <mymyry1999gmail.com>
2022-05-06 09:01:32 +0200

    AO-674: Replaced date textfield to datepicker

---
+++ src/main/resources/db/migration/20220506070019629__update_date_textfield_to_datepickers.sql
@@ -0,0 +1,7 @@
+UPDATE reports.template_parameters
+SET displayname='Data'
+WHERE id = '7f94dcd1-6bdf-4216-9185-0a8f84388016';
+
+UPDATE reports.template_parameters
+SET description='datepickers'
+WHERE id = '7f94dcd1-6bdf-4216-9185-0a8f84388016';
